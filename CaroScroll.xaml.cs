4ecb270521157bf2abc50596ec0adf00b70b3744
MSFTJim <jamesvac@live.com>
2025-01-10 10:42:06 -0500

    Applied Carousel to all 3 wheels and tightened up the UI in places.

--- CaroScroll.xaml.cs
+++ CaroScroll.xaml.cs
@@ -6,9 +6,28 @@ namespace PFBv01;
 
 public partial class CaroScroll : ContentPage
 {
-	public CaroScroll()
+    public List<string> ImageList { get; set; }
+    public CaroScroll()
 	{
         InitializeComponent();
+
+        ImageList = new List<string>
+            {
+                "swipe0.png",
+                "swipe1.png",
+                "swipe2.png",
+                "swipe3.png",
+                "swipe4.png",
+                "swipe5.png",
+                "swipe6.png",
+                "swipe7.png",
+                "swipe8.png",
+                "swipe9.png"
+            };
+
+        CaroView1.ItemsSource = ImageList;
+        CaroView2.ItemsSource = ImageList;
+        CaroView3.ItemsSource = ImageList;
     }
 
     private void MakeGuess_Click(object sender, EventArgs e)
