2bededab4d149d369f15375bf717b1a33eb02f19
MSFTJim <jamesvac@live.com>
2025-01-13 19:37:02 -0500

    added charts to stats page for better visualization.  using MicroCharts control

--- Stats.xaml.cs
+++ Stats.xaml.cs
@@ -1,3 +1,8 @@
+
+using Microcharts;
+using Microcharts.Maui;
+using SkiaSharp;
+
 namespace PFBv01;
 
 public partial class Stats : ContentPage
@@ -5,111 +10,50 @@ public partial class Stats : ContentPage
     public Stats()
     {
         InitializeComponent();
+
     }
 
     protected override void OnAppearing()
     {
         base.OnAppearing();
-        UpdateStats();
+        ShowStats();
     }
 
-    private void UpdateStats()
+    private void ShowStats()
     {
+        ChartWinsLosses();
+        ChartWinTimes();
+    }
+
+    private void ChartWinsLosses()
+    {
+        // Get the number of wins and losses from the Preferences
         int games = Preferences.Default.Get(Constants.Games, 0);
         int wins = Preferences.Default.Get(Constants.Wins, 0);
         int losses = Preferences.Default.Get(Constants.Losses, 0);
 
-        var gamesLabel = this.FindByName<Label>("GamesLabel");
-        var winsLabel = this.FindByName<Label>("WinsLabel");
-        var lossesLabel = this.FindByName<Label>("LossesLabel");
-
-        if (gamesLabel != null)
+        ChartEntry[] wins_losses = new[]
         {
-            gamesLabel.Text = $"Games: {games}";
-        }
+            new ChartEntry(wins)
+            {
+                Label = "Wins",
+                ValueLabel = wins.ToString(),
+                ValueLabelColor = SKColor.Parse("#7DDA58"),
+                Color = SKColor.Parse("#7DDA58")
+            },
+            new ChartEntry(losses)
+            {
+                Label = "Losses",
+                ValueLabel = losses.ToString(),
+                ValueLabelColor = SKColor.Parse("#D20103"),
+                Color = SKColor.Parse("#D20103")
+            }
+        };
 
-        if (winsLabel != null)
+        chartWinLoss.Chart = new PieChart
         {
-            winsLabel.Text = $"Wins: {wins}";
-        }
-
-        if (lossesLabel != null)
-        {
-            lossesLabel.Text = $"Losses: {losses}";
-        }
-
-        // Update the labels for guesses in various tries
-        var guessIn1TryLabel = this.FindByName<Label>("GuessIn1TryLabel");
-        var guessIn2TriesLabel = this.FindByName<Label>("GuessIn2TriesLabel");
-        var guessIn3TriesLabel = this.FindByName<Label>("GuessIn3TriesLabel");
-        var guessIn4TriesLabel = this.FindByName<Label>("GuessIn4TriesLabel");
-        var guessIn5TriesLabel = this.FindByName<Label>("GuessIn5TriesLabel");
-        var guessIn6TriesLabel = this.FindByName<Label>("GuessIn6TriesLabel");
-        var guessIn7TriesLabel = this.FindByName<Label>("GuessIn7TriesLabel");
-        var guessIn8TriesLabel = this.FindByName<Label>("GuessIn8TriesLabel");
-        var guessIn9TriesLabel = this.FindByName<Label>("GuessIn9TriesLabel");
-        var guessIn10TriesLabel = this.FindByName<Label>("GuessIn10TriesLabel");
-
-        if (guessIn1TryLabel != null)
-        {
-            int guessIn1Try = Preferences.Default.Get(Constants.GuessIn1Try, 0);
-            guessIn1TryLabel.Text = $"Guess in 1 try: {guessIn1Try}";
-        }
-
-        if (guessIn2TriesLabel != null)
-        {
-            int guessIn2Tries = Preferences.Default.Get(Constants.GuessIn2Tries, 0);
-            guessIn2TriesLabel.Text = $"Guess in 2 tries: {guessIn2Tries}";
-        }
-
-        if (guessIn3TriesLabel != null)
-        {
-            int guessIn3Tries = Preferences.Default.Get(Constants.GuessIn3Tries, 0);
-            guessIn3TriesLabel.Text = $"Guess in 3 tries: {guessIn3Tries}";
-        }
-
-        if (guessIn4TriesLabel != null)
-        {
-            int guessIn4Tries = Preferences.Default.Get(Constants.GuessIn4Tries, 0);
-            guessIn4TriesLabel.Text = $"Guess in 4 tries: {guessIn4Tries}";
-        }
-
-        if (guessIn5TriesLabel != null)
-        {
-            int guessIn5Tries = Preferences.Default.Get(Constants.GuessIn5Tries, 0);
-            guessIn5TriesLabel.Text = $"Guess in 5 tries: {guessIn5Tries}";
-        }
-
-        if (guessIn6TriesLabel != null)
-        {
-            int guessIn6Tries = Preferences.Default.Get(Constants.GuessIn6Tries, 0);
-            guessIn6TriesLabel.Text = $"Guess in 6 tries: {guessIn6Tries}";
-        }
-
-        if (guessIn7TriesLabel != null)
-        {
-            int guessIn7Tries = Preferences.Default.Get(Constants.GuessIn7Tries, 0);
-            guessIn7TriesLabel.Text = $"Guess in 7 tries: {guessIn7Tries}";
-        }
-
-        if (guessIn8TriesLabel != null)
-        {
-            int guessIn8Tries = Preferences.Default.Get(Constants.GuessIn8Tries, 0);
-            guessIn8TriesLabel.Text = $"Guess in 8 tries: {guessIn8Tries}";
-        }
-
-        if (guessIn9TriesLabel != null)
-        {
-            int guessIn9Tries = Preferences.Default.Get(Constants.GuessIn9Tries, 0);
-            guessIn9TriesLabel.Text = $"Guess in 9 tries: {guessIn9Tries}";
-        }
-
-        if (guessIn10TriesLabel != null)
-        {
-            int guessIn10Tries = Preferences.Default.Get(Constants.GuessIn10Tries, 0);
-            guessIn10TriesLabel.Text = $"Guess in 10 tries: {guessIn10Tries}";
-        }
-
+            Entries = wins_losses
+        };
     }
 
     private async void ResetStats_Click(object sender, EventArgs e)
@@ -119,7 +63,107 @@ public partial class Stats : ContentPage
         {
             Preferences.Default.Clear();
             await DisplayAlert("Reset Successful", "Your game statistics have been reset.", "OK");
-            UpdateStats();
+            ShowStats();
         }
     }
+
+    private void FakeStats_Clicked(object sender, EventArgs e)
+    {        
+        Preferences.Default.Set(Constants.GuessIn1Try, 0);
+        Preferences.Default.Set(Constants.GuessIn2Tries, 2);
+        Preferences.Default.Set(Constants.GuessIn3Tries, 3);
+        Preferences.Default.Set(Constants.GuessIn4Tries, 5);
+        Preferences.Default.Set(Constants.GuessIn5Tries, 6);
+        Preferences.Default.Set(Constants.GuessIn6Tries, 8);
+        Preferences.Default.Set(Constants.GuessIn7Tries, 11);
+        Preferences.Default.Set(Constants.GuessIn8Tries, 14);
+        Preferences.Default.Set(Constants.GuessIn9Tries, 9);
+        Preferences.Default.Set(Constants.GuessIn10Tries, 21);
+
+    }
+
+    private void ChartWinTimes()
+    {
+        int guessIn1Try = Preferences.Default.Get(Constants.GuessIn1Try, 0);
+        int guessIn2Tries = Preferences.Default.Get(Constants.GuessIn2Tries, 0);
+        int guessIn3Tries = Preferences.Default.Get(Constants.GuessIn3Tries, 0);
+        int guessIn4Tries = Preferences.Default.Get(Constants.GuessIn4Tries, 0);
+        int guessIn5Tries = Preferences.Default.Get(Constants.GuessIn5Tries, 0);
+        int guessIn6Tries = Preferences.Default.Get(Constants.GuessIn6Tries, 0);
+        int guessIn7Tries = Preferences.Default.Get(Constants.GuessIn7Tries, 0);
+        int guessIn8Tries = Preferences.Default.Get(Constants.GuessIn8Tries, 0);
+        int guessIn9Tries = Preferences.Default.Get(Constants.GuessIn9Tries, 0);
+        int guessIn10Tries = Preferences.Default.Get(Constants.GuessIn10Tries, 0);
+
+        ChartEntry[] GuessEntries = new[]
+        {
+            new ChartEntry(guessIn1Try)
+            {
+                Label = "1 Try",
+                ValueLabel = guessIn1Try.ToString(),
+                Color = SKColor.Parse("#0000ff")
+            },
+            new ChartEntry(guessIn2Tries)
+            {
+                Label = "2 Tries",
+                ValueLabel = guessIn2Tries.ToString(),
+                Color = SKColor.Parse("#0000ff")
+            },
+            new ChartEntry(guessIn3Tries)
+            {
+                Label = "3 Tries",
+                ValueLabel = guessIn3Tries.ToString(),
+                Color = SKColor.Parse("#0000ff")
+            },
+            new ChartEntry(guessIn4Tries)
+            {
+                Label = "4 Tries",
+                ValueLabel = guessIn4Tries.ToString(),
+                Color = SKColor.Parse("#0000ff")
+            },
+            new ChartEntry(guessIn5Tries)
+            {
+                Label = "5 Tries",
+                ValueLabel = guessIn5Tries.ToString(),
+                Color = SKColor.Parse("#0000ff")
+            },
+            new ChartEntry(guessIn6Tries)
+            {
+                Label = "6 Tries",
+                ValueLabel = guessIn6Tries.ToString(),
+                Color = SKColor.Parse("#0000ff")
+            },
+            new ChartEntry(guessIn7Tries)
+            {
+                Label = "7 Tries",
+                ValueLabel = guessIn7Tries.ToString(),
+                Color = SKColor.Parse("#0000ff")
+            },
+            new ChartEntry(guessIn8Tries)
+            {
+                Label = "8 Tries",
+                ValueLabel = guessIn8Tries.ToString(),
+                Color = SKColor.Parse("#0000ff")
+            },
+            new ChartEntry(guessIn9Tries)
+            {
+                Label = "9 Tries",
+                ValueLabel = guessIn9Tries.ToString(),
+                Color = SKColor.Parse("#0000ff")
+            },
+            new ChartEntry(guessIn10Tries)
+            {
+                Label = "10 Tries",
+                ValueLabel = guessIn10Tries.ToString(),
+                Color = SKColor.Parse("#0000ff")
+            }
+        };
+
+        chartGuesses.Chart = new BarChart
+        {
+            Entries = GuessEntries
+        };
+    }
+
+
 }
